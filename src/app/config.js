cdde9b0ccf36c310bad0f2b19957c0ccbe855e6b
Vinod Kumar <vkviswa@gmail.com>
2019-05-01 11:36:54 +0530

    remove db configs

--- src/app/config.js
+++ src/app/config.js
@@ -2,12 +2,12 @@ export const firebaseConfiguration = [
   {
     appName: 'default',
     configuration: {
-      apiKey: "AIzaSyDHW9dNluVmRMn9fvtiz4l34ZBbDWsOuBg",
-      authDomain: "testapp-6ab58.firebaseapp.com",
-      databaseURL: "https://testapp-6ab58.firebaseio.com",
-      projectId: "testapp-6ab58",
-      storageBucket: "testapp-6ab58.appspot.com",
-      messagingSenderId: "498353385934"
+      apiKey: "***********************************",
+      authDomain: "*******************************",
+      databaseURL: "*************************************",
+      projectId: "********************************",
+      storageBucket: "***********************************",
+      messagingSenderId: "********************"
     }
   }]
   // , {
